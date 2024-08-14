c894b1f20a68f5855a13884bedda33bb146936cf
Rebeca Ruiz <rebeca.ruiz.guardia0102@gmail.com>
2024-08-13 21:52:32 -0500

    Modificaciones mejoradas en el archivo de javaScript del proyecto

--- script.js
+++ script.js
@@ -1,89 +1,75 @@
 let txtArea = document.getElementById('texto_ingresado');
-let txtEncriptado = document.getElementById('texto_encriptado');
+let mensaje = document.getElementById('mensaje');
 let sectionMensaje = document.querySelector('.section__mensaje');
 let imagen = document.querySelector('.section__mensaje__img');
 let tituloMensaje = document.querySelector('.section__mensaje__titulo');
 let btnCopiar = document.querySelector('.boton-copiar');
 
+// Ajuste de tamaño del text-area
 txtArea.addEventListener("input", e => {
     console.log(txtArea.scrollHeight);
     txtArea.style.height = '400px';
     txtArea.style.height = txtArea.scrollHeight + 'px';
-
-    console.log(txtArea.scrollHeight);
 });
 
-let arrayTextoInicial = [];
-let textoEncriptado = [];
-let textoDesencriptado = [];
-
-function encriptar() {
-    let textoInicial = txtArea.value;
-    txtEncriptado.value = ''
-    arrayTextoInicial = [...textoInicial];
-
-    for (var i = 0; i < arrayTextoInicial.length; i++) {
-        if (arrayTextoInicial[i] == 'a') {
-            arrayTextoInicial[i] = 'ai'
-        } else if (arrayTextoInicial[i] == 'e') {
-            arrayTextoInicial[i] = 'enter'
-        } else if (arrayTextoInicial[i] == 'i') {
-            arrayTextoInicial[i] = 'imes'
-        } else if (arrayTextoInicial[i] == 'o') {
-            arrayTextoInicial[i] = 'ober'
-        } else if (arrayTextoInicial[i] == 'u') {
-            arrayTextoInicial[i] = 'ufat'
-        }
-    }
-
-    if (imagen && tituloMensaje) {
-        imagen.remove();
-        tituloMensaje.remove();
-        txtEncriptado.style.cssText = 'margin-top: 0; margin-bottom: 3.2rem; font-size: 2.4rem; text-align: left';
-        sectionMensaje.style.cssText = 'justify-content: space-between;';
-        btnCopiar.removeAttribute('hidden');
-    }
-    
-    textoEncriptado = arrayTextoInicial.join('');
-    txtEncriptado.innerHTML = textoEncriptado;
-    limpiar();
+// Evento de botón encriptar
+function btnEncriptar() {
+    validarTextoIngresado(txtArea.value);
+    let textoEncriptado = remplazarTexto(txtArea.value, 0, 1);
+    mensaje.innerHTML = textoEncriptado;
+    cambiarEstilos();
 }
 
-function limpiar() {
-    txtArea.value = '';
+// Evento de botón desencriptar
+function btnDesencriptar() {
+    validarTextoIngresado(txtArea.value);
+    let textoDesencriptado = remplazarTexto(txtArea.value, 1, 0);
+    mensaje.innerHTML = textoDesencriptado;
+    cambiarEstilos();
 }
 
-function desencriptar() {
-    if (txtArea.value != '') {
-        for (var i = 0; i < arrayTextoInicial.length; i++) {
-            if (arrayTextoInicial[i] == 'ai') {
-                arrayTextoInicial[i] = 'a'
-            } else if (arrayTextoInicial[i] == 'enter') {
-                arrayTextoInicial[i] = 'e'
-            } else if (arrayTextoInicial[i] == 'imes') {
-                arrayTextoInicial[i] = 'i'
-            } else if (arrayTextoInicial[i] == 'ober') {
-                arrayTextoInicial[i] = 'o'
-            } else if (arrayTextoInicial[i] == 'ufat') {
-                arrayTextoInicial[i] = 'u'
-            }
-        }
-    }
-    textoDesencriptado = arrayTextoInicial.join('');
-    txtEncriptado.innerHTML = textoDesencriptado;
-    limpiar();
-}
-
+// Función para copiar el texto del mensaje
 function copiarTexto() {
     let range = document.createRange();
-    range.selectNodeContents(txtEncriptado);
+    range.selectNodeContents(mensaje);
     let selection = window.getSelection();
     selection.removeAllRanges();
     selection.addRange(range);
 
-    navigator.clipboard.writeText(txtEncriptado.innerHTML).then(function() {
-        console.log('Texto copiado: ' + txtEncriptado.innerHTML);
+    navigator.clipboard.writeText(mensaje.innerHTML).then(function() {
+        console.log('Texto copiado: ' + mensaje.innerHTML);
     }).catch(function(error) {
-        alert('Hubo un error al copiar el texto: ' + txtEncriptado.innerHTML);
+        alert('Hubo un error al copiar el texto: ' + mensaje.innerHTML);
     });
+}
+
+// Función que valida el texto ingresado, que no sean mayúsculas ni con tilde
+function validarTextoIngresado(texto) {
+    if (!/^[a-z\s]*$/.test(texto)) {
+        alert('El texto no debe contener letras mayúsculas ni tildes.');
+        return;
+    }
+}
+
+// Función para reeemplazar textos
+function remplazarTexto(stringEncriptada, posicionInicial, posicionFinal) {
+    let matrizCodigo = [['e', 'enter'], ['i', 'imes'], ['a', 'ai'], ['o', 'ober'], ['u', 'ufat']];
+    stringEncriptada = stringEncriptada.toLowerCase();
+
+    for(let i = 0; i < matrizCodigo.length; i++) {
+        if (stringEncriptada.includes(matrizCodigo[i][posicionInicial])) {
+            stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][posicionInicial], matrizCodigo[i][posicionFinal]);
+        }
+    }
+    return stringEncriptada;
+}
+
+// Función para cambiar estilos
+function cambiarEstilos() {
+    txtArea.value = "";
+    imagen.remove();
+    tituloMensaje.remove();
+    mensaje.style.cssText = 'margin-top: 0; margin-bottom: 3.2rem; font-size: 2.4rem; text-align: left; width: 100%';
+    sectionMensaje.style.cssText = 'justify-content: space-between;';
+    btnCopiar.removeAttribute('hidden');
 }
